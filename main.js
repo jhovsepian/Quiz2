e6f251558d772c7d0904d7e696971502b74711e3
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2015-04-29 09:05:16 -0700

    Quiz 2 :)

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+ 
+	$( ".dan" ).click(function() {
+    	alert( "Hey you clicked on me" );
+	});
+
+    $( "#iwao" ).click(function() {
+    	$( "#iwao" ).hide();
+	});
+
+};
